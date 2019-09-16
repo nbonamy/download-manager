4109a733ee93d414fe876cd4673fb869fed8fc4f
Nicolas Bonamy <nicolas@bonamy.fr>
2019-09-16 13:03:21 -0500

    movie title extraction

---
+++ src/test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import unittest
+import utils
+
+# test data
+filenames = {
+  'Interstellar.mkv': 'Interstellar',
+  'Interstellar.avi': 'Interstellar',
+  'Interstellar.720p.mkv': 'Interstellar',
+  'Interstellar.MULTI.mkv': 'Interstellar',
+  'Interstellar.VOSTFR.mkv': 'Interstellar',
+  'Interstellar.VOST.mkv': 'Interstellar',
+  'Interstellar.2016.mkv': 'Interstellar',
+  'Interstellar.MULTI.1080p.mkv': 'Interstellar',
+  'Interstellar.2016.1080p.mkv': 'Interstellar',
+  'Interstellar.2016.MULTI.mkv': 'Interstellar',
+  'Blade.Runner.mkv': 'Blade Runner',
+  'Blade.Runner.avi': 'Blade Runner',
+  'Blade.Runner.720p.mkv': 'Blade Runner',
+  'Blade.Runner.MULTI.mkv': 'Blade Runner',
+  'Blade.Runner.VOSTFR.mkv': 'Blade Runner',
+  'Blade.Runner.VOST.mkv': 'Blade Runner',
+  'Blade.Runner.2016.mkv': 'Blade Runner',
+  'Blade.Runner.MULTI.1080p.mkv': 'Blade Runner',
+  'Blade.Runner.2016.1080p.mkv': 'Blade Runner',
+  'Blade.Runner.2016.MULTI.mkv': 'Blade Runner',
+  'Blade.Runner.2016.MULTI.1080p.mkv': 'Blade Runner',
+  'Blade.Runner.1984.MULTI.1080p.mkv': 'Blade Runner',
+  'Blade.Runner.2049.mkv': 'Blade Runner',
+  'Blade.Runner.2049.MULTI.1080p.mkv': 'Blade Runner',
+  'Blade.Runner.2049.1080p.mkv': 'Blade Runner',
+  'Blade.Runner.2049.2017.MULTI.1080p.mkv': 'Blade Runner 2049',
+  'C\'est.comme.ça.2018.FRENCH.720p.mkv': 'C\'est comme ça',
+}
+
+# run test
+class TestParseFilenameMethod(unittest.TestCase):
+
+  def testFilename(self):
+    for filename in filenames:
+      title = utils.extractTitle(filename)
+      self.assertEqual(filenames[filename], title)
+
+if __name__ == '__main__':
+  unittest.main()
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,41 @@
+import re
 from math import log2
 
 _suffixes = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
 def humansize(size):
-	# determine binary order in steps of size 10
-	# (coerce to int, // still returns a float)
-	order = int(log2(size) / 10) if size else 0
-	# format file size
-	# (.4g results in rounded numbers for exact matches and max 3 decimals,
-	# should never resort to exponent values)
-	return '{:.2g} {}'.format(size / (1 << (order * 10)), _suffixes[order])
+  # determine binary order in steps of size 10
+  # (coerce to int, // still returns a float)
+  order = int(log2(size) / 10) if size else 0
+  # format file size
+  # (.4g results in rounded numbers for exact matches and max 3 decimals,
+  # should never resort to exponent values)
+  return '{:.2g} {}'.format(size / (1 << (order * 10)), _suffixes[order])
+
+def extractTitle(filename):
+
+  # first split to get each part
+  parts = filename.split('.')
+
+  # now qualify each part
+  # their last occurence is the file extension
+  lastDate = len(parts)-1
+  lastLang = len(parts)-1
+  lastFormat = len(parts)-1
+
+  # scan
+  for i in range(len(parts)):
+    part = parts[i].lower()
+    if re.match(r'^[0-9][0-9][0-9][0-9]$', part):
+      lastDate = i
+    if part in ['multi', 'vo', 'vf', 'vost', 'vostfr', 'french', 'truefrench']:
+      lastLang = i
+    if part in ['sd','hd','bluray','720p','1080p','2160p','4k']:
+      lastFormat = i
+
+  # first non title
+  titleEnd = min(lastDate, lastLang, lastFormat)
+
+  # join
+  parts = parts[:titleEnd]
+  return ' '.join(parts)
